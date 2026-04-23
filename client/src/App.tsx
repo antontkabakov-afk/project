ab171bce74b16db63e97de9447680a884714869e
ATKabakov22 <antontkabakov@gmail.com>
2026-04-23 22:14:00 +0300

    feat: wire auth routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,18 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import PortfolioLayout from "./components/portfolio-layout";
 import Assets from "./pages/Assets";
 import History from "./pages/History";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Statistics from "./pages/Statistics";
 import Wallet from "./pages/Wallet";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate replace to="/wallet" />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route element={<PortfolioLayout />}>
         <Route path="/assets" element={<Assets />} />
         <Route path="/history" element={<History />} />
