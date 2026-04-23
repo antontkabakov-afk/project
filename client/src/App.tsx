1baac60d53446fc436c5138439831b448fb8171c
ATKabakov22 <antontkabakov@gmail.com>
2026-04-23 22:06:00 +0300

    List supported crypto assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import PortfolioLayout from "./components/portfolio-layout";
+import Assets from "./pages/Assets";
 import History from "./pages/History";
 import Statistics from "./pages/Statistics";
 import Wallet from "./pages/Wallet";
@@ -10,6 +11,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Navigate replace to="/wallet" />} />
       <Route element={<PortfolioLayout />}>
+        <Route path="/assets" element={<Assets />} />
         <Route path="/history" element={<History />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="/wallet" element={<Wallet />} />
