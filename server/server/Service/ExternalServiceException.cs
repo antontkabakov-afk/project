a0784d0ae59c74a749dcc21d4036f9b3a09a6622
ATKabakov22 <antontkabakov@gmail.com>
2026-04-23 22:33:00 +0300

    feat: add portfolio analytics service

---
+++ server/server/Service/ExternalServiceException.cs
@@ -0,0 +1,12 @@
+namespace server.Service;
+
+public class ExternalServiceException : Exception
+{
+    public ExternalServiceException(string message, Exception? innerException = null, int? statusCode = null)
+        : base(message, innerException)
+    {
+        StatusCode = statusCode;
+    }
+
+    public int? StatusCode { get; }
+}
